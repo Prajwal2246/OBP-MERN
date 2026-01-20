db31fdf34e392a39826b1d1c97b609e773a4360a
Prajwal2246 <janbandhuprajwal28@gmail.com>
2026-01-20 16:03:03 +0530

    useState,virtualDom,fragment,

--- Lecture66-State Basics (useState)/stateBasics/src/components/FormDetails.jsx
+++ Lecture66-State Basics (useState)/stateBasics/src/components/FormDetails.jsx
@@ -29,71 +29,94 @@ function FormDetails() {
 
   return (
     <>
-      <form
-        onSubmit={handleFormSubmit}
-        className="flex flex-col justify-center border p-2 m-2"
-      >
-        <label htmlFor="name">Name</label>
-        <input
-          type="text"
-          id="name"
-          value={name}
-          onChange={(e) => {
-            setName(e.target.value);
-          }}
-          placeholder="enter name"
-          className="outline-none bg-gray-300  rounded-2xl px-4 py-2 mb-3"
-        />
-        <label htmlFor="age">Age</label>
-        <input
-          type="text"
-          id="age"
-          value={age}
-          placeholder="enter age"
-          className="outline-none bg-gray-300  rounded-2xl px-4 py-2 mb-3"
-          onChange={(e) => {
-            setAge(e.target.value);
-          }}
-        />
-        <div className="flex gap-2 mb-3">
-          <label>Gender</label>
-          <input
-            type="radio"
-            value="male"
-            checked={gender == "male"}
-            onChange={(e) => {
-              setGender(e.target.value);
-            }}
-          />
-          Male
-          <input
-            type="radio"
-            value="female"
-            checked={gender == "female"}
-            onChange={(e) => {
-              setGender(e.target.value);
-            }}
-          />
-          Female
-        </div>
-        <button
-          type="submit"
-          className="outline-none bg-blue-500 transition hover:scale-105 text-white font-semibold  px-4 py-2 rounded-2xl self-center"
-        >
-          Submit
-        </button>
-      </form>
+      <div
+        className="fixed inset-0 -z-10
+    bg-gradient-to-br from-slate-200 via-slate-300 to-slate-200
+    blur-2xl"
+      />
 
-      {formDetails &&
-        formDetails.map((obj, idx) => {
-          return (
-            <div key={idx} className="border flex flex-col gap-1 rounded-xl">
-              <h1>{obj.name}</h1>
-              <p>{obj.age}</p>
-              <p>{obj.gender}</p>
+      <div className="min-h-[70vh] flex flex-col items-center justify-center gap-10 px-4">
+        <form
+          onSubmit={handleFormSubmit}
+          className="w-full max-w-md
+        bg-white
+        rounded-xl shadow-xl
+        p-8 flex flex-col gap-5"
+        >
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            id="name"
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+            placeholder="enter name"
+            className="outline-none bg-slate-300  rounded-md px-4 py-2 mb-3"
+          />
+          <label htmlFor="age">Age</label>
+          <input
+            type="text"
+            id="age"
+            value={age}
+            placeholder="enter age"
+            className="outline-none bg-slate-300  rounded-md px-4 py-2 mb-3"
+            onChange={(e) => {
+              setAge(e.target.value);
+            }}
+          />
+          <div className="flex gap-2 mb-3">
+            <label>Gender</label>
+            <input
+              type="radio"
+              value="male"
+              checked={gender == "male"}
+              onChange={(e) => {
+                setGender(e.target.value);
+              }}
+            />
+            Male
+            <input
+              type="radio"
+              value="female"
+              checked={gender == "female"}
+              onChange={(e) => {
+                setGender(e.target.value);
+              }}
+            />
+            Female
+          </div>
+          <button
+            type="submit"
+            className="outline-none bg-black w-full transition hover:scale-105 text-white font-semibold  py-3 rounded-md self-center"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+
+      <div className="flex flex-wrap gap-4 p-12 w-[700px]">
+        {formDetails.map((obj, idx) => (
+          <div
+            key={idx}
+            className="bg-white 
+            border border-slate-200
+            rounded-xl p-4 w-56
+            shadow-sm"
+          >
+            <div className="flex flex-col space-y-1 min-full">
+              <div className="flex justify-between w-full">
+                <h3 className="font-medium text-slate-800">Name:</h3>
+                <span className="text-slate-800">{obj.name.toUpperCase()}</span>
+              </div>
+              <p className="text-sm text-slate-600">Age: {obj.age}</p>
+              <p className="text-sm text-slate-600 capitalize">
+                Gender: {obj.gender}
+              </p>
             </div>
-          );
-        })}
+          </div>
+        ))}
+      </div>
     </>
   );
 }
